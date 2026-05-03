7eb73c9bcbd710bde6e7c1b1a9a51ed867c4e9d4
jhonatas_dev23 <258070573+jhonatas23@users.noreply.github.com>
2026-05-03 14:48:41 +0000

    fiz o desafio nível avançado em C.

--- xadrez.c
+++ xadrez.c
@@ -1,54 +1,64 @@
 #include <stdio.h>
 
-
-int main() {
-  // Entrada dos dados
-    int torre, bispo = 1, rainha = 1;
-    int movimentoCavalo = 1;
-
-    printf("Torre:\n");
-  // loop for
-    for (torre = 1; torre <= 5; torre++)
-    {   // saída de dados
-        // torre anda 5 casa para a direita
-        printf("Direita\n");
-    }
-
-    printf("\n");
-    printf("Bispo:\n");
-  // loop while
-    while (bispo <= 5)
-    {   // saída de dados
-        // bispo anda 5 casa na diagonal para direita
-        printf("Cima, Direita\n");
-        bispo++;
-    }
-  // loop do-while
-    printf("\n");
-    printf("Rainha:\n");
-    do
-    {   // saída de dados
-        // rainha anda 8 casas para a esquerda
-        printf("Esquerda\n");
-        rainha++;
-    } while (rainha <= 8);
-    // loop aninhado
-    printf("\n");
-    printf("Cavalo:\n");
-    while (movimentoCavalo < 2)
-    {
-      for (int i = 0; i < 2; i++)
-      {
-        printf("Cima\n");
-      }
-      printf("Esquerda\n");
-        movimentoCavalo++;
-    }
-
-    
-
-    
-    
-
-    return 0;
+// Recursividade Básica
+void moverTorre(int casas){
+  if(casas > 0){
+    printf("Direita\n");
+    moverTorre(casas - 1);
+  }
 }
+
+// Recursividade + Loop Aninhado, que faz o movimento em diagonal do Bispo
+void moverBispo(int casas){
+  for (int i = 0; i < casas; i++)
+  {
+    for(int j = 0; j < 1; j++){
+      printf("Direita\n");
+      printf("Cima\n");
+    }  
+  }
+}
+
+// Recursividade Básica
+void moverRainha(int casas){
+  if(casas > 0){
+    printf("Esquerda\n");
+    moverRainha(casas - 1);
+  }
+}
+
+// Recursividade + Loop Aninhado, que faz o CLÁSSICO movimento em "L" do Cavalo
+void moverCavalo(int casas){
+  for(int i = 0; i < casas; i++)
+  {
+    for(int j = 0; j < 2; j++)
+    {
+      printf("Cima\n");
+    }
+      printf("Direita\n");
+  }
+}
+
+int main(){
+
+  // Saída dos Dados
+  printf("\n");
+  printf("Torre\n");
+  moverTorre(5);
+
+  printf("\n");
+  printf("Bispo:\n");
+  moverBispo(5);
+
+  printf("\n");
+  printf("Rainha:\n");
+  moverRainha(8);
+
+  printf("\n");
+  printf("Cavalo:\n");
+  moverCavalo(1);
+
+ // Fim do programa.
+  return 0;
+}
+
